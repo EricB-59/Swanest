f03123e92f8af38e088dcdae5312c0dc0e007553
EricB-59 <ebaenac59@gmail.com>
2025-02-22 19:46:35 +0100

    User create function done

--- backend/src/Controller/UserController.php
+++ backend/src/Controller/UserController.php
@@ -2,8 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use DateTimeImmutable;
+use DateTimeZone;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 #[Route('/user', name: 'app_user')]
@@ -15,5 +21,46 @@ final class UserController extends AbstractController
         return $this->json([
             'message' => 'Welcome to your user controller',
         ]);
+    }
+
+    #[Route('/create', name: 'app_user_create', methods: ['POST'])]
+    public function createUser(Request $request, EntityManagerInterface $entityManager): JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
+
+        $username = $data['username'];
+        $email = $data['email'];
+        $password = $data['password'];
+
+        if (empty($username) || empty($email) || empty($password)) {
+            return new JsonResponse('Empty fields', Response::HTTP_BAD_REQUEST);
+        }
+
+        $repeatedEmail = $entityManager->getRepository(User::class)->findBy(['email' => $email]);
+        if ($repeatedEmail) {
+            return new JsonResponse('Repeted Email', Response::HTTP_BAD_REQUEST);
+        }
+
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            return new JsonResponse('Invalid email', Response::HTTP_BAD_REQUEST);
+        }
+
+        $regexPattern = '^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$^';
+        if (!preg_match($regexPattern, $password)) {
+            return new JsonResponse('Invalid password', Response::HTTP_BAD_REQUEST);
+        }
+
+        $user = new User();
+        $user->setUsername($username);
+        $user->setEmail($email);
+        $user->setPassword($password);
+
+        $dateTimeZone = new DateTimeZone("Europe/Madrid");
+        $user->setCreatedAt(new DateTimeImmutable(timezone: $dateTimeZone));
+
+        $entityManager->persist($user);
+        $entityManager->flush();
+
+        return new JsonResponse('User: ' . $user->getUsername() . ' created', Response::HTTP_OK);
     }
 }
