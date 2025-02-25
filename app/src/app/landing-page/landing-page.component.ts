7f3427ff0bc7579d61e956815d389b09e3d792c9
GuillemS <guim.san.oli@gmail.com>
2025-02-25 17:46:56 +0100

    Update of the frontend with angular

--- app/src/app/landing-page/landing-page.component.ts
+++ app/src/app/landing-page/landing-page.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { ContactComponent } from './components/contact/contact.component';
 
 @Component({
   selector: 'app-landing-page',
-  imports: [],
+  imports: [ContactComponent],
   templateUrl: './landing-page.component.html',
-  styleUrl: './landing-page.component.css'
+  styleUrl: './landing-page.component.css',
 })
-export class LandingPageComponent {
-
-}
+export class LandingPageComponent {}
