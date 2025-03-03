db9c2269362e2c3fcc6b87fe9e24bd9e87d567c4
EricB-59 <ebaenac59@gmail.com>
2025-03-03 10:24:20 +0100

    #23 Implements scroll snap on landing page

--- app/src/app/landing-page/landing-page.component.ts
+++ app/src/app/landing-page/landing-page.component.ts
@@ -1,8 +1,17 @@
 import { Component } from '@angular/core';
+import { LandingComponent } from './components/landing/landing.component';
+import { ApplicationComponent } from './components/application/application.component';
+import { NewsComponent } from './components/news/news.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 @Component({
   selector: 'app-landing-page',
-  imports: [],
+  imports: [
+    LandingComponent,
+    ApplicationComponent,
+    NewsComponent,
+    ContactComponent,
+  ],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.css',
 })
