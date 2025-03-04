009d3a8ddd7aea5fb6a319999e24613768a361eb
EricB-59 <ebaenac59@gmail.com>
2025-03-04 17:49:59 +0100

    #25 Update profile and implement feedback entity

--- backend/src/Entity/Profile.php
+++ backend/src/Entity/Profile.php
@@ -36,6 +36,12 @@ class Profile
     #[ORM\ManyToOne]
     private ?Province $province = null;
 
+    #[ORM\Column]
+    private ?\DateTimeImmutable $created_at = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $updated_at = null;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -128,6 +134,30 @@ class Profile
     public function setProvince(?Province $province): static
     {
         $this->province = $province;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->created_at;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $created_at): static
+    {
+        $this->created_at = $created_at;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeImmutable
+    {
+        return $this->updated_at;
+    }
+
+    public function setUpdatedAt(\DateTimeImmutable $updated_at): static
+    {
+        $this->updated_at = $updated_at;
 
         return $this;
     }
