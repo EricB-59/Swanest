74379e929e6e462436fc20fe405953d2ce0682c3
GuillemS <guim.san.oli@gmail.com>
2025-03-11 16:02:26 +0100

    #20 implemented the functionality for feedback in the contact form of the landing page

--- app/src/app/services/feedback/feedback.service.ts
+++ app/src/app/services/feedback/feedback.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { User } from '../../models/user';
+import { Feedback } from '../../models/feedback';
 
 @Injectable({
   providedIn: 'root',
@@ -10,4 +10,12 @@ export class FeedbackService {
   private apiUrl = 'http://localhost:8000/feedback';
 
   constructor(private connHttp: HttpClient) {}
+
+  add(feedback: Feedback): Observable<object> {
+    return this.connHttp.post(this.apiUrl + '/add', feedback, {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+      }),
+    });
+  }
 }
