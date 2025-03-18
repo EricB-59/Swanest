2a7c371693320b0585c3844f01f771717ae6ff4c
Jowi <157366219+JowiMesas@users.noreply.github.com>
2025-03-18 16:37:21 +0100

    28-fixed-login

--- backend/src/Repository/UserRepository.php
+++ backend/src/Repository/UserRepository.php
@@ -15,7 +15,14 @@ class UserRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, User::class);
     }
-
+    public function findByEmailOrUsername($value) : ?User {
+        return $this->createQueryBuilder('u')
+            ->andWhere('u.email = :val')
+            ->orWhere('u.username = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult();
+    }
     //    /**
     //     * @return User[] Returns an array of User objects
     //     */
