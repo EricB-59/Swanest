e3adfcb2d87219f7e46db7a354781ab8b118c9e1
Jowi <157366219+JowiMesas@users.noreply.github.com>
2025-03-18 16:16:14 +0100

    #29 modal login and register without design

--- app/src/app/landing-page/auth/login-user/login-user.component.ts
+++ app/src/app/landing-page/auth/login-user/login-user.component.ts
@@ -1,15 +1,72 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserService } from '../../../services/user/user.service';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  FormsModule,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-login-user',
-  imports: [],
+  imports: [ReactiveFormsModule],
   template: `
-    <p>
-      login-user works!
-    </p>
+    <section>
+      <form
+        [formGroup]="form"
+        (submit)="login()"
+        class="custom-form flex flex-col items-center gap-10 font-basereg"
+      >
+        <h1 class=" font-basebold text-3xl">LOGIN</h1>
+        <input
+          class="border-2 border-gray-400 rounded-2xl p-2"
+          type="text"
+          id="identifier"
+          formControlName="identifier"
+          placeholder="Usuario/Email"
+        />
+        <input
+          class="border-2 border-gray-400 rounded-2xl p-2"
+          type="password"
+          id="password"
+          formControlName="password"
+          placeholder="Introduce tu contraseña"
+        />
+        <button
+          class=" bg-black text-white rounded-xl p-3 pr-10 pl-10 cursor-pointer"
+          type="submit"
+        >
+          LOGIN
+        </button>
+      </form>
+    </section>
   `,
-  styles: ``
+  styles: ``,
 })
-export class LoginUserComponent {
-
+export class LoginUserComponent implements OnInit {
+  constructor(private router: Router) {}
+  exist: boolean = false;
+  identifier: any = '';
+  password: any = '';
+  userService: UserService = inject(UserService);
+  form = new FormGroup({
+    identifier: new FormControl(''),
+    password: new FormControl(''),
+  });
+  ngOnInit(): void {}
+  login() {
+    this.exist = false;
+    this.identifier = this.form.value.identifier;
+    this.password = this.form.value.password;
+    this.userService.login(this.identifier, this.password).subscribe(
+      (result) => {
+        console.log(result);
+        this.exist = true;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
 }
