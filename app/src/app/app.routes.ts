406a5d84a1e70a99beeac3ca43228ece4e5c5db9
GuillemS <guim.san.oli@gmail.com>
2025-03-28 16:44:20 +0100

    #64 fix typografies bugs and responsive part

--- app/src/app/app.routes.ts
+++ app/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { PersonalInfoComponent } from './landing-page/profile/personal-info/personal-info.component';
 import { ProfileInfoComponent } from './landing-page/profile/profile-info/profile-info.component';
+import { ImagesUploadComponent } from './landing-page/profile/images-upload/images-upload.component';
 
 export const routes: Routes = [
   { path: '', component: LandingPageComponent },
   { path: 'personal', component: PersonalInfoComponent },
   { path: 'profile', component: ProfileInfoComponent },
+  { path: 'images', component: ImagesUploadComponent },
 ];
