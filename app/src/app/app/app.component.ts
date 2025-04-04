809ac257a4686d0d76ae932edb99749fe948c966
EricB-59 <ebaenac59@gmail.com>
2025-04-04 19:16:13 +0200

    fixes

--- app/src/app/app/app.component.ts
+++ app/src/app/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatchSectionComponent } from './match-section/match-section.component';
+import { UpdateUserComponent } from './auth/update-user/update-user.component';
 
 @Component({
   selector: 'app-app',
-  imports: [MatchSectionComponent],
+  imports: [UpdateUserComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
