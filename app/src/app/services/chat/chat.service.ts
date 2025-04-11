051ef1cfa9d372ac52ac60dcc91cea5a4be591f6
EricB-59 <ebaenac59@gmail.com>
2025-04-11 12:16:04 +0200

    New API_URL externalitation

--- app/src/app/services/chat/chat.service.ts
+++ app/src/app/services/chat/chat.service.ts
@@ -2,18 +2,19 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { User } from '../../models/user';
+import { API_URL } from '../../../config/const';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ChatService {
-  private apiUrl = 'http://localhost:8000/chat';
+  private PREFIX = 'chat';
 
   constructor(private connHttp: HttpClient) {}
 
   // ! Chat section - List of chats
   getChats(id: number): Observable<object> {
-    return this.connHttp.get(this.apiUrl + `/${id}`);
+    return this.connHttp.get(API_URL + this.PREFIX + '/' + id);
   }
 
   // ? Chat - Chat user to user
