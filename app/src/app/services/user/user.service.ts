098261d5987a06e99f7f1a277a66602568a5184d
EricB-59 <ebaenac59@gmail.com>
2025-04-29 11:39:35 +0200

    #164 Function to create preferences on the first entry

--- app/src/app/services/user/user.service.ts
+++ app/src/app/services/user/user.service.ts
@@ -4,6 +4,23 @@ import { Observable } from 'rxjs';
 import { User } from '../../models/user';
 import { API_URL } from '../../../config/const';
 
+interface ImagesToSend {
+  id: number;
+  image_1: string;
+  image_2: string;
+  image_3: string;
+  image_4: string;
+  image_5: string;
+}
+
+export interface Gender {
+  name: string;
+}
+
+export interface Province {
+  name: string;
+}
+
 @Injectable({
   providedIn: 'root',
 })
@@ -36,5 +53,24 @@ export class UserService {
   login(identifier: string, password: string): Observable<User> {
     const data = { identifier, password };
     return this.connHttp.post<User>(API_URL + this.PREFIX + '/login', data);
+  }
+
+  images(id: number, images: any) {
+    const imagesWithId = { id: id, ...images };
+    console.log(images);
+    return this.connHttp.post<boolean>(
+      API_URL + this.PREFIX + '/images',
+      imagesWithId,
+    );
+  }
+
+  preferences(
+    user_id: number,
+    province: Province,
+    genre: Gender,
+    birthDate: string,
+  ) {
+    const preferences = { user_id, province, genre, birthDate };
+    return this.connHttp.post<boolean>(API_URL + '/preferences', preferences);
   }
 }
